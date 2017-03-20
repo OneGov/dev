22ef5d5769e50080f3dfc71b3b62f7c1657bda96
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2017-03-20 11:10:02 +0100

    Use a custom bootstrap.py

--- .travis-install.sh
+++ .travis-install.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env sh
-pip install --upgrade pip
-pip install --upgrade setuptools
-
 if [ "$ES_URL" != 'no' ]; then
     mkdir /tmp/elasticsearch
     wget -O - "$ES_URL" | tar xz --directory=/tmp/elasticsearch --strip-components=1
