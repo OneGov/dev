459d0fdfb0f9a6a3393d44004319854c1bf87ae6
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2019-07-17 11:43:11 +0200

    Try updating java alternatives

--- .travis.sh
+++ .travis.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
+sudo update-java-alternatives --jre-headless --jre --set java-1.8.0-openjdk-amd64
+
 if [ "$ES_URL" != 'no' ]; then
     mkdir /tmp/elasticsearch
     wget -O - "$ES_URL" | tar xz --directory=/tmp/elasticsearch --strip-components=1
